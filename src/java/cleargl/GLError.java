0d8aea3331dbd70004b1c8dbc621c1f6145844f9
Ulrik Günther <ulrik@42degreesoffreedom.com>
2015-12-14 12:53:07 +0100

    Removed unneccessary GL error checking
    * ClearVolume/ClearGL has lots of code where OpenGL is queried for errors several
      times per frame. This is unneccessary and removed with this commit. If strict
      OpenGL error checking is required, set the system property `ClearGL.CheckForOpenGLErrors`.

--- src/java/cleargl/GLError.java
+++ src/java/cleargl/GLError.java
@@ -11,13 +11,17 @@ public class GLError
 	public static final String printGLErrors(	final GL lGL,
 																						String lDescription)
 	{
-		final int lGLErrorCode = lGL.glGetError();
-		final String lGLErrorStr = sGLU.gluErrorString(lGLErrorCode);
-		if (lGLErrorCode != 0)
-			System.err.format("OPENGL ERROR %s #%d : %s \n",
-												lDescription,
-												lGLErrorCode,
-												lGLErrorStr);
-		return lGLErrorStr;
+		if(System.getProperty("ClearGL.CheckForOpenGLErrors") != null) {
+			final int lGLErrorCode = lGL.glGetError();
+			final String lGLErrorStr = sGLU.gluErrorString(lGLErrorCode);
+			if (lGLErrorCode != 0)
+				System.err.format("OPENGL ERROR %s #%d : %s \n",
+						lDescription,
+						lGLErrorCode,
+						lGLErrorStr);
+			return lGLErrorStr;
+		} else {
+			return "";
+		}
 	}
 }
