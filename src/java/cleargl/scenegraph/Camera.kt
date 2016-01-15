0c9f6431a29f77a529131f19a29cf42b559cd459
Ulrik Günther <ulrik@42degreesoffreedom.com>
2016-01-15 15:37:31 +0100

    Added getWorldOrientation to Camera

--- src/java/cleargl/scenegraph/Camera.kt
+++ src/java/cleargl/scenegraph/Camera.kt
@@ -1,6 +1,7 @@
 @file:JvmName("Camera")
 package cleargl.scenegraph
 
+import cleargl.GLMatrix
 import cleargl.GLVector
 
 open class Camera : Node("Camera") {
@@ -8,10 +9,21 @@ open class Camera : Node("Camera") {
     var targeted = false
     var active = false
 
-    protected var target: GLVector = GLVector(floatArrayOf(0.0f, 0.0f, 0.0f))
+    protected var target: GLVector = GLVector(0.0f, 0.0f, 0.0f)
+    protected var forward: GLVector = GLVector(0.0f, 1.0f, 0.0f)
 
     init {
         this.nodeType = "Camera"
     }
 
+    fun getWorldOrientation(): GLVector {
+        if(targeted) {
+            return (target - position!!).getNormalized()
+        }
+        else {
+            val v: GLVector = GLVector(forward.x(), forward.y(), forward.z(), 0.0f)
+            //FIXME: val o: GLVector= GLMatrix.fromQuaternion(rotation).mult(v)
+            return GLVector(1.0f, 0.0f, 0.0f)
+        }
+    }
 }
